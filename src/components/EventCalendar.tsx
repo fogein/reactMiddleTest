ae2ad90ccb3c2251dcd53eae8393423d30a88516
fogein <77330295+fogein@users.noreply.github.com>
2022-09-03 14:36:05 +0200

    fix

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -4,17 +4,20 @@ import { EventsType } from './EventForm'
 import { Moment } from 'moment';
 import { formatDate } from '../utils';
 
-export const EventCalendar:React.FC<{events:EventsType[]}> = (props) => {
+export const EventCalendar: React.FC<{ events: EventsType[] }> = (props) => {
 
-  
-  const dateCellRender = (value:Moment) => {
+
+  const dateCellRender = (value: Moment) => {
     let formatedDate = formatDate(value.toDate())
-    const a = props.events.filter(ev => ev.date=== formatedDate )
+    const a = props.events.filter(ev => ev.date === formatedDate)
     return (
       <div>
         {a.map((e) => {
           return (
-            <div>{e.description}</div>
+            <>
+              <div><b>Event for:</b>{e.guests}</div>
+              <div><b>Description</b>{e.description}</div>
+            </>
           )
         })}
       </div>
@@ -22,7 +25,7 @@ export const EventCalendar:React.FC<{events:EventsType[]}> = (props) => {
   }
   return (
     <Calendar
-    dateCellRender={dateCellRender}
+      dateCellRender={dateCellRender}
     />
   )
 }
